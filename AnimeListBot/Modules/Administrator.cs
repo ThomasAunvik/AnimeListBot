92c4fd86d96a643e9ad8a65169b62fbad86b9422
Thomas Aunvik <thaun1@live.no>
2018-12-01 14:44:51 +0100

    Added Administrator setstatus

--- AnimeListBot/Modules/Administrator.cs
+++ AnimeListBot/Modules/Administrator.cs
@@ -51,5 +51,56 @@ namespace AnimeListBot.Modules
                 await embed.SendMessage(Context.Channel);
             }
         }
+
+        [Command("setgamestatus")]
+        [Summary(
+            "Setting Game Status for the bot" +
+            "\n    Playing = 0," +
+            "\n    Streaming = 1," +
+            "\n    Listening = 2," +
+            "\n    Watching = 3"
+        )]
+        public async Task SetGameStatus(Discord.ActivityType activityType, string gameMessage)
+        {
+            EmbedHandler embed = new EmbedHandler(Context.User, "Set game status to: " + Enum.GetName(typeof(Discord.ActivityType), activityType) + " " + gameMessage);
+
+            if (Program.botOwners.Contains(Context.User.Id.ToString()))
+            {
+                await Program._client.SetGameAsync(gameMessage, null, activityType);
+                await embed.SendMessage(Context.Channel);
+            }
+            else
+            {
+                embed.Title = "You dont have permission to do this command.";
+                await embed.SendMessage(Context.Channel);
+            }
+        }
+
+        [Command("setonlinestatus")]
+        [Summary(
+            "Setting User Status for the Bot" +
+            "\n    Status:" +
+            "\n    Offline = 0," +
+            "\n    Online = 1," +
+            "\n    Idle = 2," +
+            "\n    AFK = 3," +
+            "\n    DoNotDisturb = 4," +
+            "\n    Invisible = 5"
+        )]
+        public async Task SetOnlineStatus(Discord.UserStatus status)
+        {
+            EmbedHandler embed = new EmbedHandler(Context.User, "Set online status to: " + Enum.GetName(typeof(Discord.UserStatus), status));
+
+            if (Program.botOwners.Contains(Context.User.Id.ToString()))
+            {
+                await Program._client.SetStatusAsync(status);
+                await embed.SendMessage(Context.Channel);
+            }
+            else
+            {
+                embed.Title = "You dont have permission to do this command.";
+                await embed.SendMessage(Context.Channel);
+            }
+        }
     }
 }

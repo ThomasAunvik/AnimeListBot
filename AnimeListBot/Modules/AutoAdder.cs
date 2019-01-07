7cd52560cd60dcabda256435902dcff716858a6f
Thomas Aunvik <thaun1@live.no>
2019-01-07 21:23:13 +0100

    Stupid Nullwhite checking was making AutoAdder ignore add

--- AnimeListBot/Modules/AutoAdder.cs
+++ AnimeListBot/Modules/AutoAdder.cs
@@ -116,7 +116,7 @@ namespace AnimeListBot.Modules
                     return;
                 }
 
-                if (isValidLink && containsMAL && string.IsNullOrWhiteSpace(user.MAL_Username))
+                if (isValidLink && containsMAL && !string.IsNullOrWhiteSpace(user.MAL_Username))
                 {
                     UserProfile profile = await Program._jikan.GetUserProfile(usernamePart);
                     if (profile != null)
@@ -126,7 +126,7 @@ namespace AnimeListBot.Modules
                         await user.UpdateMALInfo();
                     }
                 }
-                else if (isValidLink && containsAnilist && string.IsNullOrWhiteSpace(user?.Anilist_Username))
+                else if (isValidLink && containsAnilist && !string.IsNullOrWhiteSpace(user?.Anilist_Username))
                 {
                     IAniUser profile = await AniUserQuery.GetUser(usernamePart);
                     if (profile != null)
